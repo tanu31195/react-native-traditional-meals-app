6b6f3113efb0e078bc228fd1b8bf09392ea3644c
tanu31195 <tanu31195@users.noreply.github.com>
2023-04-08 16:04:52 +0530

    Location picket component

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,12 +1,101 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
+
+import OutlinedButton from "../UI/OutlinedButton";
+import { COLORS } from "../../constants";
+import { getMapPreview } from "../../util/location";
 
 export default function LocationPicker() {
+  const [pickedLocation, setPickedLocation] = useState();
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permission!",
+        "You need to grant location permissions to use the Travel Diary"
+      );
+      return false;
+    }
+
+    return true;
+  }
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
+  }
+
+  function pickOnMapHandler() {}
+
+  let locationPreview = <Text>No location picked yet.</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.mapPreviewImage}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
+    );
+  }
   return (
     <View>
-      <Text>LocationPicker</Text>
+      <View style={styles.mapPreview}>{locationPreview}</View>
+      <View style={styles.actions}>
+        <OutlinedButton icon='location' onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon='map' onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: "100%",
+    height: 250,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    backgroundColor: COLORS.melon,
+    marginVertical: 10,
+    borderColor: COLORS.tertiary,
+    borderWidth: 2,
+    overflow: 'hidden'
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  mapPreviewImage: {
+    width: "100%",
+    height: "100%",
+  },
+});
